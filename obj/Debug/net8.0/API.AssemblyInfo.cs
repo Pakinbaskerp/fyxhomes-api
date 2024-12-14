dbdf06d9513a1ef19c90036b46b23ff68459a416
Pakin <pakin@propelinc.com>
2024-12-14 14:03:44 +0530

    Initial commit

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d41ed24131146193de7962994333f7ffc9153f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e11b8162e49ffdf817f0afc8d5d484c2135668")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
